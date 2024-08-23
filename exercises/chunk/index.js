60b3ed5353b4e13acd9df6fd4dd1205638f12526
leVert <selmamathis15@gmail.com>
2024-08-23 12:03:23 +0300

    chunk: slice method

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -10,19 +10,29 @@
 
 function chunk(array, size) {
     const chunked=[];
-
-    for(let element of array){
-        last = chunked[chunked.length-1];
-
-        if(!last || last.length===size){
-            chunked.push([element]);
-        }else{
-            last.push(element);
-        }
+    let index=0;
+    while(index<array.length){
+        chunked.push(array.slice(index, index+size));
+        index+=size;
 
     }
-    return chunked
-
+    return chunked;
+   
 }
 
 module.exports = chunk;
+
+
+// const chunked=[];
+
+// for(let element of array){
+//     last = chunked[chunked.length-1];
+
+//     if(!last || last.length===size){
+//         chunked.push([element]);
+//     }else{
+//         last.push(element);
+//     }
+
+// }
+// return chunked
